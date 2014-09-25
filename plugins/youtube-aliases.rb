6e28e79126e8066f1125c163c751a5aa7a62bcfd
Flandoo <devnull@localhost>
2014-09-24 21:20:53 -0400

    New aliases. I'll take these out of the repo soon because they are personal

--- plugins/youtube-aliases.rb
+++ plugins/youtube-aliases.rb
@@ -7,7 +7,7 @@ class Elevator < Plugin
   end
 
   def go(source, args, bot)
-    bot.commands['youtube'].go(source, @elevator_tunes.sample.split(" "), bot)
+    bot.commands['youtube'].go(source, @elevator_tunes.sample.split(' '), bot)
   end
 end
 
@@ -20,6 +20,19 @@ class HateTrain < Plugin
   end
 
   def go(source, args, bot)
-    bot.commands['youtube'].go(source, @hate_tunes.sample.split(" "), bot)
+    bot.commands['youtube'].go(source, @hate_tunes.sample.split(' '), bot)
+  end
+end
+
+class JamesOhShit < Plugin
+  def initialize
+    @help_text = "Shiiiiit"
+    @commands = ['james', 'jeb']
+    @song = ['zi44BM1YHf8']
+    super
+  end
+  
+  def go(source, args, bot)
+    bot.commands['youtube'].go(source, @song.sample.split(' '), bot)
   end
 end
