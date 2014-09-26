dae017e6162c3ed97bd3a095d8221e34037515fc
Flandoo <devnull@localhost>
2014-09-26 16:24:56 -0400

    Style fixes in plugins.rb

--- plugins.rb
+++ plugins.rb
@@ -1,10 +1,10 @@
 class Plugin
   attr_accessor :help_text, :enabled, :protected, :response, :min_args, :needs_sanitization, :commands
-  
+
   def self.plugins
     @plugins ||= []
   end
-  
+
   def self.inherited(klass)
     @plugins ||= []
     @plugins << klass
@@ -15,7 +15,7 @@ class Plugin
     @min_args ||= 0
     @enabled = true if @enabled.nil?
     @protected ||= false # does nothing yet
-    @help_text ||= "No help text included for this command"
+    @help_text ||= 'No help text included for this command'
     @response ||= :auto
   end
 
@@ -32,16 +32,16 @@ class Say < Plugin
     super
   end
 
-  def go(source, args, bot)
-    bot.say_to_channel(bot.current_channel, args.join(" "))
-  end 
+  def go(_source, args, bot)
+    bot.say_to_channel(bot.current_channel, args.join(' '))
+  end
 end
 
 class Whisper < Plugin
   def initialize
     @min_args = 2
     @commands = ['whisper']
-    @help_text = "Send a whisper from robocop to target user - whisper [user] [message]"
+    @help_text = 'Send a whisper from robocop to target user - whisper [user] [message]'
     super
   end
 
@@ -61,7 +61,7 @@ class Roll < Plugin
   def initialize
     @min_args = 1
     @commands = ['roll']
-    @help_text = "Roll an x sided die - roll [sides]"
+    @help_text = 'Roll an x sided die - roll [sides]'
     super
   end
 
@@ -74,8 +74,8 @@ end
 class Youtube < Plugin
   def initialize
     @needs_sanitization = true
-    @commands = ['youtube', 'yt']
-    @help_text = "Play a youtube video - youtube [url]"
+    @commands = %w(youtube yt)
+    @help_text = 'Play a youtube video - youtube [url]'
     @min_args = 1
     super
   end
@@ -85,17 +85,6 @@ class Youtube < Plugin
     if result
       system('mpc play')
       bot.say(self, source, 'Request successful. Loading...')
-      # failed attempt at autoseeking below
-=begin
-      if args[1]
-        args.delete_at(0)
-        puts args.to_s
-        seek_result = false
-        until seek_result
-          seek_result = bot.commands['seek'].go(source, args, bot)
-        end
-      end
-=end
     else
       bot.say(self, source, 'Failed to play video. Check given url.')
     end
@@ -105,8 +94,8 @@ end
 class Soundcloud < Plugin
   def initialize
     @needs_sanitization = true
-    @commands = ['soundcloud', 'sc']
-    @help_text = "Play a soundcloud song - soundcloud [url]"
+    @commands = %w(soundcloud sc)
+    @help_text = 'Play a soundcloud song - soundcloud [url]'
     @min_args = 1
     super
   end
@@ -125,23 +114,23 @@ end
 class Fuck < Plugin
   def initialize
     @min_args = 1
-    @help_text = "Fuck a user anonymously - fuck [name]"
+    @help_text = 'Fuck a user anonymously - fuck [name]'
     @commands = ['fuck']
     super
   end
 
   def go(source, args, bot)
     if args[0] == $USERNAME
-      bot.say(self, source, "No, fuck you.")
+      bot.say(self, source, 'No, fuck you.')
     else
-      bot.commands['whisper'].go(source, [args[0], "Someone anonymously says fuck you."], bot)
+      bot.commands['whisper'].go(source, [args[0], 'Someone anonymously says fuck you.'], bot)
     end
   end
 end
 
 class Volume < Plugin
   def initialize
-    @help_text = "Change the volume, 0 - 100 - volume [[level]]. No params = check the volume"
+    @help_text = 'Change the volume, 0 - 100 - volume [[level]]. No params = check the volume'
     @commands = ['volume']
     @max_volume = 100
     super
@@ -149,9 +138,9 @@ class Volume < Plugin
 
   def go(source, args, bot)
     if args[0]
-      if args[0][0] == "-"
+      if args[0][0] == '-'
         new_volume = bot.bot.player.volume - args[0].to_i.abs
-      elsif args[0][0] == "+"
+      elsif args[0][0] == '+'
         new_volume = bot.bot.player.volume + args[0].to_i.abs
       else
         new_volume = args[0].to_i
@@ -170,55 +159,55 @@ end
 
 class Clear < Plugin
   def initialize
-    @help_text = "Stops anything playing, and clears the current playlist"
+    @help_text = 'Stops anything playing, and clears the current playlist'
     @commands = ['clear']
     super
   end
 
-  def go(source, args, bot)
+  def go(_source, _args, _bot)
     system('mpc clear')
   end
 end
 
 class Next < Plugin
   def initialize
-    @help_text = "Advances to the next song in the queue."
-    @commands = ['next', 'advance']
+    @help_text = 'Advances to the next song in the queue.'
+    @commands = %w(next advance)
     super
   end
 
-  def go(source, args, bot)
+  def go(_source, _args, _bot)
     system('mpc next')
   end
 end
 
 class GetDateTime < Plugin
   def initialize
-    @help_text = "Prints out the date and time"
-    @commands = ['date', 'time']
+    @help_text = 'Prints out the date and time'
+    @commands = %w(date time)
     super
   end
 
-  def go(source, args, bot)
+  def go(source, _args, bot)
     bot.say(self, source, Time.now.to_s)
   end
 end
 
 class Commands < Plugin
   def initialize
-    @help_text = "Prints out all available commands"
+    @help_text = 'Prints out all available commands'
     @commands = ['commands']
     super
   end
 
-  def go(source, args, bot)
+  def go(source, _args, bot)
     bot.say(self, source, bot.commands.keys.to_s)
   end
 end
 
 class Help < Plugin
   def initialize
-    @help_text = "Gives help about a specific command - help [command]"
+    @help_text = 'Gives help about a specific command - help [command]'
     @commands = ['help']
     super
   end
@@ -226,9 +215,9 @@ class Help < Plugin
   def go(source, args, bot)
     if args[0]
       if bot.commands[args[0]].nil?
-        bot.say(self, source, "Sorry, command you requested help on is not found.")
+        bot.say(self, source, 'Sorry, command you requested help on is not found.')
       elsif bot.commands[args[0]].help_text.empty?
-        bot.say(self, source, "Sorry, command has no set help text.")
+        bot.say(self, source, 'Sorry, command has no set help text.')
       else
         bot.say(self, source, bot.commands[args[0]].help_text)
       end
@@ -240,30 +229,28 @@ end
 
 class Goto < Plugin
   def initialize
-    @help_text = "Send robocop away to another channel. Poor robocop :("
+    @help_text = 'Send robocop away to another channel. Poor robocop :('
     @min_args = 1
     @commands = ['goto']
     super
   end
 
   def go(source, args, bot)
-    begin
-      bot.bot.join_channel(args[0])
-    rescue
-      bot.say(self, source, "Failed to join that channel. Check permissions / if that channel exists.")
-    end
+    bot.bot.join_channel(args[0])
+  rescue
+    bot.say(self, source, 'Failed to join that channel. Check permissions / if that channel exists.')
   end
 end
 
 class Seek < Plugin
   def initialize
-    @help_text = "Seek to x seconds in the currently playing media - seek [seconds]"
+    @help_text = 'Seek to x seconds in the currently playing media - seek [seconds]'
     @min_args = 1
     @commands = ['seek']
     super
   end
 
-  def go(source, args, bot)
-    system("mpc", "seek", args[0])
+  def go(_source, args, _bot)
+    system('mpc', 'seek', args[0])
   end
 end
