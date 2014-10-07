b9a257cdcc0f0067d3909f9433feee70ffc38b3e
Flandoo <devnull@localhost>
2014-10-07 16:50:47 -0400

    Fix roll plugin

--- plugins/roll.rb
+++ plugins/roll.rb
@@ -4,14 +4,14 @@ class Roll < Plugin
   def initialize
     @min_args = 1
     @commands = ['roll']
-    @help_text = 'Roll some dice - see the dicebag ruby gem online for formatting.'
+    @help_text = 'Roll some dice - 4d5 - see the dicebag ruby gem online for formatting.'
     super
   end
 
   def go(source, args, bot)
     result = DiceBag::Roll.new(args.join(' ')).result
-      rescue DiceBag::DiceBagError
-        bot.say(self, source, 'Error parsing roll command. Numbers must be less than 4 digits.')
-        bot.say(self, source, result.to_s)
+    bot.say(self, source, result.to_s)
+    rescue DiceBag::DiceBagError
+      bot.say(self, source, 'Error parsing roll command. Format = \'xdy\'. Numbers must be less than 4 digits.')
   end
 end
