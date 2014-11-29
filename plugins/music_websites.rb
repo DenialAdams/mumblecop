aae4197f6b9de9162d07ca1061b849233e3e9a74
Flandoo <devnull@localhost>
2014-11-29 14:11:35 -0500

    Remove get_soundcloud system call, fixing #42

--- plugins/music_websites.rb
+++ plugins/music_websites.rb
@@ -9,9 +9,7 @@ class Youtube < Plugin
 
   def go(source, args, bot)
     begin
-      result = `youtube-dl --prefer-insecure -i -f140 -g "#{args[0]}"`
-      result.chomp!
-      puts [result].to_s
+      result = `youtube-dl --prefer-insecure -i -f140 -g "#{args[0]}"`.chomp
       bot.mpd.add(result)
       bot.mpd.play if bot.mpd.stopped?
       bot.say(self, source, 'Request successful. Loading...')
@@ -32,13 +30,14 @@ class Soundcloud < Plugin
   end
 
   def go(source, args, bot)
-    result = system('get_soundcloud', args[0])
-    if result
+    begin
+      result = `youtube-dl --prefer-insecure -i -g "#{args[0]}"`.chomp
+      bot.mpd.add(result)
       bot.mpd.play if bot.mpd.stopped?
-      bot.say(self, source, 'Request successful. Please wait a few moments for the source to begin streaming.')
+      bot.say(self, source, 'Request successful. Loading...')
       bot.mpd.seek(args[1].to_i) if args[1]
     else
-      bot.say(self, source, 'Failed to stream song. Check given url.')
+      bot.say(self, source, 'Failed to stream song. Check given url and seek parameter (if given.)')
     end
   end
 end
