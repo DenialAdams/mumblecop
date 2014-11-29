f8ef6928db5c2ae772a35ee7eab1a8a3109ce315
Flandoo <devnull@localhost>
2014-11-29 12:14:49 -0500

    Bot was not reading address from config, always using localhost

--- bot.rb
+++ bot.rb
@@ -21,7 +21,7 @@ class MumbleBot
   def initialize
     @plugins = []
     @commands = {}
-    @bot = Mumble::Client.new('localhost') do |conf|
+    @bot = Mumble::Client.new(CONFIG['address']) do |conf|
       conf.username = CONFIG['username']
       conf.password = CONFIG['password'] if CONFIG['password']
     end
