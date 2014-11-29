68e2657c82f7b66445a71ea494d29ac40b5fbbb2
Flandoo <devnull@localhost>
2014-11-29 14:21:41 -0500

    Fix begin/rescue block in soundcloud

--- plugins/music_websites.rb
+++ plugins/music_websites.rb
@@ -8,15 +8,13 @@ class Youtube < Plugin
   end
 
   def go(source, args, bot)
-    begin
-      result = `youtube-dl --prefer-insecure -i -f140 -g "#{args[0]}"`.chomp
-      bot.mpd.add(result)
-      bot.mpd.play if bot.mpd.stopped?
-      bot.say(self, source, 'Request successful. Loading...')
-      bot.mpd.seek(args[1].to_i) if args[1]
-    rescue
-      bot.say(self, source, 'Failed to play video. Check given url and seek paramater (if given.)')
-    end
+    result = `youtube-dl --prefer-insecure -i -f140 -g "#{args[0]}"`.chomp
+    bot.mpd.add(result)
+    bot.mpd.play if bot.mpd.stopped?
+    bot.say(self, source, 'Request successful. Loading...')
+    bot.mpd.seek(args[1].to_i) if args[1]
+  rescue
+    bot.say(self, source, 'Failed to play video. Check given url and seek paramater (if given.)')
   end
 end
 
@@ -30,14 +28,12 @@ class Soundcloud < Plugin
   end
 
   def go(source, args, bot)
-    begin
-      result = `youtube-dl --prefer-insecure -i -g "#{args[0]}"`.chomp
-      bot.mpd.add(result)
-      bot.mpd.play if bot.mpd.stopped?
-      bot.say(self, source, 'Request successful. Loading...')
-      bot.mpd.seek(args[1].to_i) if args[1]
-    else
-      bot.say(self, source, 'Failed to stream song. Check given url and seek parameter (if given.)')
-    end
+    result = `youtube-dl --prefer-insecure -i -g "#{args[0]}"`.chomp
+    bot.mpd.add(result)
+    bot.mpd.play if bot.mpd.stopped?
+    bot.say(self, source, 'Request successful. Loading...')
+    bot.mpd.seek(args[1].to_i) if args[1]
+  rescue
+    bot.say(self, source, 'Failed to stream song. Check given url and seek parameter (if given.)')
   end
 end
