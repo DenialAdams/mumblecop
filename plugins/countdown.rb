3f1d54614647503f09915c42dad8be7cbdf9c512
Flandoo <devnull@localhost>
2014-11-29 11:14:11 -0500

    Add interval feature for countdown, fixing #60

--- plugins/countdown.rb
+++ plugins/countdown.rb
@@ -8,9 +8,14 @@ class Countdown < Plugin
 
   def go(source, args, bot)
     end_number = args[0].to_i
+    if args[1]
+      interval = args[1].to_i
+    else
+      interval = 1
+    end
     if end_number > 0
       end_number.downto(1) do |i|
-        bot.say(self, source, i.to_s)
+        bot.say(self, source, i.to_s) if i % interval == 0
         sleep(1)
       end
       bot.say(self, source, 'STOP')
