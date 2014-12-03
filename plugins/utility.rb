d567ff31b64d74be7445a488026cc30ada1e0121
Flandoo <devnull@localhost>
2014-12-03 16:41:55 -0500

    Add command to retrieve the hash of a user

--- plugins/utility.rb
+++ plugins/utility.rb
@@ -9,3 +9,16 @@ class GetDateTime < Plugin
     bot.say(self, source, Time.now.to_s)
   end
 end
+
+class GetUserHash < Plugin
+  def initialize
+    super
+    @help_text = "Gets the hash of a user's certificate, for verifying identity - hash [username]"
+    @commands = %w(hash)
+    @min_args = 1
+  end
+
+  def go(source, args, bot)
+    bot.say(self, source, "The certificate hash for #{args[0]} is #{bot.bot.find_user(args[0]).hash}")
+  end
+end
