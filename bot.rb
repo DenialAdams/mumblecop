0aa06f94cc079f3e9a0244c473571c786d07cfd1
Flandoo <devnull@localhost>
2015-03-18 16:40:39 -0400

    Change to method to non self-altering to fix crashing

--- bot.rb
+++ bot.rb
@@ -117,7 +117,7 @@ class MumbleBot
       puts "#{get_username_from_id(message.actor)}: #{contents}"
     end
     return if contents.strip.empty?
-    possible_commands = contents.split(';').reject!(&:blank?)
+    possible_commands = contents.split(';').reject(&:blank?)
     possible_commands.each_with_index do |command, i|
       next if i == 0 || matches_trigger(command.split(' ')[0])
       possible_commands[i - 1] = possible_commands[i - 1].concat(';').concat(command)
