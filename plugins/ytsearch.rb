65faecd1e22de5114189895ba88acee814adf9a7
Flandoo <devnull@localhost>
2015-03-23 19:33:02 -0400

    Refactor youtube search plugin

--- plugins/ytsearch.rb
+++ plugins/ytsearch.rb
@@ -3,27 +3,26 @@ class Search < Plugin
     super
     @help_text = 'Search a video on youtube and play the first result - search [query]'
     @min_args = 1
-    @commands = %w(ytsearch)
+    @commands = %w(ytsearch yts)
   end
 
   def go(source, args, bot)
     result = nil
-    name = 'No title'
     error = 'No youtube-dl error'
-    Open3.popen3('youtube-dl', '--prefer-insecure', '-i', '-f140', '-q', '--no-warnings', '-g', '--default-search', 'ytsearch:', args.join(' ')) do |_stdin, stdout, stderr|
+    Open3.popen3('youtube-dl', '--prefer-insecure', '-i', '-f140', '-q', '--no-warnings', '-ge', '--default-search', 'ytsearch:', args.join(' ')) do |_stdin, stdout, stderr|
       result = stdout.read.chomp
       error = stderr.read.chomp
     end
-    bot.mpd.add(result)
+    result = result.split('\n')
+    puts result.to_s
+    bot.mpd.add(result[1])
     bot.mpd.play if bot.mpd.stopped?
-    bot.say(self, source, 'Request successful. Loading...')
-    Open3.popen3('youtube-dl', '--prefer-insecure', '-i', '-f140', '-q', '--no-warnings', '-e', '--default-search', 'ytsearch:', args.join(' ')) do |_stdin, stdout, _stderr|
-      name = stdout.read.chomp
-    end
-    bot.say(self, source, 'Added "' + name + '".')
-  rescue => e
-    bot.say(self, source, 'Failed to play video. Try modifying your query.')
-    bot.say(self, source, error)
-    bot.say(self, source, e.message)
+    bot.say(self, source, 'Added "' + result[0] + '".')
+    bot.mpd.play if bot.mpd.stopped?
+    rescue => e
+      bot.say(self, source, 'Failed to play video. Try modifying your query.')
+      bot.say(self, source, error)
+      bot.say(self, source, e.message)
+      return 1
   end
 end
