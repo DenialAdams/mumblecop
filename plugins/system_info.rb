2f8e718f054e6dfdf5073b4904731e9115699d2a
Flandoo <devnull@localhost>
2015-03-25 22:05:28 -0400

    Silently fail if can't get mpd / youtube-dl version

--- plugins/system_info.rb
+++ plugins/system_info.rb
@@ -10,10 +10,15 @@ class SystemInfo < Plugin
     bot.say(self, source, `uname -a`) unless Gem.win_platform?
     bot.say(self, source, RUBY_DESCRIPTION)
     return unless CONFIG['use-mpd']
-    bot.say(self, source, `mpd --version`.lines[0])
+    begin
+      bot.say(self, source, `mpd --version`.lines[0])
+    rescue
+      puts "ERROR: Failed to get mpd version"
+    end
     begin
       bot.say(self, source, 'Youtube-dl: ' + `youtube-dl --version`)
     rescue
+     puts "ERROR: Failed to get youtube-dl version"
     end
   end
 end
