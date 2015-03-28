bc76b3a8ff753d29a8809c99b6ad91f4e3b9e042
Flandoo <devnull@localhost>
2015-03-28 12:22:22 -0400

    Remove redundancy from ytsearch plugin

--- plugins/ytsearch.rb
+++ plugins/ytsearch.rb
@@ -18,7 +18,6 @@ class Search < Plugin
     bot.mpd.send_command('addtagid', bot.mpd.queue.last.id, 'title', result[0])
     bot.mpd.play if bot.mpd.stopped?
     bot.say(self, source, 'Added "' + result[0] + '".')
-    bot.mpd.play if bot.mpd.stopped?
     rescue => e
       bot.say(self, source, 'Failed to play video. Try modifying your query.')
       bot.say(self, source, error)
